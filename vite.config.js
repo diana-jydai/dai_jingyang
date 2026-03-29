fa958cab476b0fa3b86774d7aa29e072ac532685
jingyang dai <diana.jydai@gmail.com>
2026-03-30 02:23:51 +0800

    chore: remove vite.config.js to avoid config conflict

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-var _a;
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-var repositoryName = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split('/')[1];
-export default defineConfig({
-    plugins: [react()],
-    base: repositoryName ? "/".concat(repositoryName, "/") : '/',
-});
